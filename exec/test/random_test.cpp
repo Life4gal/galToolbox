e95777b1865c72873ba688a303575ba918590d35
life4gal <life4gal@gmail.com>
2021-04-22 00:16:36 +0800

    random

--- exec/test/random_test.cpp
+++ exec/test/random_test.cpp
@@ -1,10 +1,75 @@
 #include <gtest/gtest.h>
 
+#include <iostream>
 #include <random/random.hpp>
+#include <random>
+#include <set>
 
 namespace {
 	TEST(TEST_RANDOM, random) {
 		using namespace gal::toolbox::random;
 
+		{
+			std::cout << "\n# 64-bit bound\n";
+			std::cout << "min: " << random256PlusPlus::min() << "\nmax: " << random256PlusPlus::max() << std::endl;
+		}
+
+		std::cout << "\n# Initialize with a 64-bit seed\n";
+		{
+			random256PlusPlus engine{};
+			for (auto i = 0; i < 10; ++i) {
+				std::cout << engine() << '\n';
+			}
+		}
+
+		std::cout << "\n# Initialize with a 64-bit seed\n";
+		{
+			random256PlusPlus engine{123456};
+			for (auto i = 0; i < 10; ++i) {
+				std::cout << engine() << '\n';
+			}
+		}
+
+		std::cout << "\n# Initialize with a given state\n";
+		{
+			random256PlusPlus engine{{1, 2, 3, 4}};
+			for (auto i = 0; i < 10; ++i) {
+				std::cout << engine() << '\n';
+			}
+		}
+
+		std::cout << "\n# Generate int in the range of [0, 100]\n";
+		{
+			random256PlusPlus				   engine{123456};
+			std::uniform_int_distribution<int> dist(0, 10);
+			for (auto i = 0; i < 5; ++i) {
+				std::cout << dist(engine) << '\n';
+			}
+		}
+
+		std::cout << "\n# Test overlap1 (100000 times) \n";
+		{
+			for (auto i = 0; i < 10; ++i) {
+				random256PlusPlus		engine{};
+				std::set<std::uint64_t> set;
+				for (auto j = 0; j < 100000; ++j) {
+					set.insert(engine());
+				}
+				std::cout << set.size() << '\n';
+			}
+		}
+
+		std::cout << "\n# Test overlap2 (100000 times) \n";
+		{
+			for (auto i = 0; i < 10; ++i) {
+				random256PlusPlus				   engine{};
+				std::uniform_int_distribution<int> dist;
+				std::set<std::uint64_t>			   set;
+				for (auto j = 0; j < 100000; ++j) {
+					set.insert(dist(engine));
+				}
+				std::cout << set.size() << '\n';
+			}
+		}
 	}
 }// namespace
