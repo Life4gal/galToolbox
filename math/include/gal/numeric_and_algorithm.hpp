c42c77016c6f25f0ae3e5ae0537e51320585e8a5
life4gal <life4gal@gmail.com>
2021-09-16 08:59:24 +0800

    math::exponential_operations

---
+++ math/include/gal/numeric_and_algorithm.hpp
@@ -0,0 +1,125 @@
+#pragma once
+
+#include <numeric>
+#include <algorithm>
+
+#include <gal/math_fwd.hpp>
+#include <gal/vector.hpp>
+#include <gal/matrix.hpp>
+
+namespace gal::toolbox::math
+{
+	template <math_type_t T>
+	constexpr math_value_type<T> sum(const T& t) noexcept
+	{
+		return [&]<std::size_t...I>(std::index_sequence<I...>)
+		{
+			return (t[I] + ...);
+		}(std::make_index_sequence<T::data_size>{});
+	}
+
+	template <math_type_t T, typename Pred, typename Projection = std::identity>
+	constexpr bool all(const T& t, Pred pred, Projection projection = {})
+			noexcept(noexcept(
+					std::ranges::all_of(
+							std::declval<const T&>(),
+							std::declval<Pred>(),
+							std::declval<Projection>()
+									)
+							))
+	{
+		return std::ranges::all_of(t, pred, projection);
+	}
+
+	template <math_type_t T>
+	requires std::is_same_v<math_value_type<T>, bool>
+			constexpr bool all(const T& t)
+					noexcept(noexcept(
+							std::ranges::all_of(
+									std::declval<const T&>(),
+									std::declval<decltype(
+											[](const bool b)
+											{
+												return b;
+											}
+											)>()
+											)
+									))
+	{
+		return std::ranges::all_of(t,
+								   [](const bool b)
+								   {
+									   return b;
+								   });
+	}
+
+	template <math_type_t T, typename Pred, typename Projection = std::identity>
+	constexpr bool any(const T& t, Pred pred, Projection projection = {})
+			noexcept(noexcept(
+					std::ranges::any_of(
+							std::declval<const T&>(),
+							std::declval<Pred>(),
+							std::declval<Projection>()
+									)
+							))
+	{
+		return std::ranges::any_of(t, pred, projection);
+	}
+
+	template <math_type_t T>
+	requires std::is_same_v<math_value_type<T>, bool>
+			constexpr bool any(const T& t)
+					noexcept(noexcept(
+							std::ranges::any_of(
+									std::declval<const T&>(),
+									std::declval<decltype(
+											[](const bool b)
+											{
+												return b;
+											}
+											)>()
+											)
+									))
+	{
+		return std::ranges::any_of(t,
+								   [](const bool b)
+								   {
+									   return b;
+								   });
+	}
+
+	template <math_type_t T, typename Pred, typename Projection = std::identity>
+	constexpr bool none(const T& t, Pred pred, Projection projection = {})
+			noexcept(noexcept(
+					std::ranges::none_of(
+							std::declval<const T&>(),
+							std::declval<Pred>(),
+							std::declval<Projection>()
+									)
+							))
+	{
+		return std::ranges::none_of(t, pred, projection);
+	}
+
+	template <math_type_t T>
+	requires std::is_same_v<math_value_type<T>, bool>
+			constexpr bool none(const T& t)
+					noexcept(noexcept(
+							std::ranges::none_of(
+									std::declval<const T&>(),
+									std::declval<decltype(
+											[](const bool b)
+											{
+												return b;
+											}
+											)>()
+											)
+									))
+	{
+		return std::ranges::none_of(t,
+									[](const bool b)
+									{
+										return b;
+									});
+	}
+}
